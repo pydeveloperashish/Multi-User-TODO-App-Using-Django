bf7f2c7ee4d82feb8201f33e547b0cd99e6cfdcb
Ashish Kushwaha <therobomarket@gmail.com>
2023-07-21 22:08:50 +0530

    signup form creation FE BE done

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, login, signup
 
 urlpatterns = [
     path('', view = home, name = 'home'),
+    path('login/', view = login, name = 'login'),
+    path('signup/', view = signup, name = 'signup'),
 ]
